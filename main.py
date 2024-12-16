8e5dc6d71f8e6176eb6658de372bd5edf773a7c3
Sultan-Shahid <sultanshahid2425@gmail.com>
2024-12-16 17:11:04 +0500

    first commit

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return "I changed the Code."
+
+
+@app.get("/sultan/cgpa")
+def read_root():
+    return "Sultan CGPA : 3.58"
+
+
+@app.get("/ali/cgpa")
+def read_root():
+    return "Ali CGPA : 3.44"
